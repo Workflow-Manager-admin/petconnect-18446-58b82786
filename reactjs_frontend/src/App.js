e1d041f55e96cae7a85e520299fa2ed777a7d521
Kavia AI <production@kavia.ai>
2025-07-02 15:08:19 +0000

    CheckPoint

--- reactjs_frontend/src/App.js
+++ reactjs_frontend/src/App.js
@@ -238,7 +238,8 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<LoginScreen />} />
             <Route path="/register" element={<RegisterScreen />} />
-            <Route path="/pets" element={<PetList />} />
+            {/* Use PetListings (API-powered) instead of demo PetList for dynamic listings */}
+            <Route path="/pets" element={<PetListings />} />
             <Route path="/pets/new" element={<PetCreateForm />} />
             {/* TODO: Add more routes/screens here */}
           </Routes>
